766d885b4da7965fea651cb2cdf26f234fa95452
Eadkennychan <Eadkennychan@gmail.com>
2017-03-09 13:38:53 +0800

    网络监测修改

--- ZWUtilityKit/Network/NetworkApply.h
+++ ZWUtilityKit/Network/NetworkApply.h
@@ -7,33 +7,10 @@
 //
 
 #import <Foundation/Foundation.h>
-#import "Reachability.h"
 
-BOOL isNetworkAvailabel()
-{
-    /*
-     *网络探测, return YES 为网络正常, return NO 为网络未连接
-     */
-    if ([[Reachability reachabilityForInternetConnection] currentReachabilityStatus] == NotReachable)
-    {
-        return NO;
-    }
-    else
-    {
-        return YES;
-    }
-}
-
-BOOL addNetworkMonitor(id observer, SEL aSelector)
-{
-    BOOL bRet = NO;
-    bRet = [[Reachability reachabilityForInternetConnection] startNotifier];
-    [[NSNotificationCenter defaultCenter] addObserver:observer selector:aSelector name:kReachabilityChangedNotification object:nil];
-    return bRet;
-}
-
-void removeNetworkMonitor(id observer)
-{
-    [[Reachability reachabilityForInternetConnection] stopNotifier];
-    [[NSNotificationCenter defaultCenter] removeObserver:observer name:kReachabilityChangedNotification object:nil];
-}
+/*
+ *  @brief: 判断网络是否可用
+ */
+FOUNDATION_EXPORT BOOL isNetworkAvailabel();
+BOOL addNetworkMonitor(id observer, SEL aSelector);
+void removeNetworkMonitor(id observer);
